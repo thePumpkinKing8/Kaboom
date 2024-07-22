cbb5c43956689d10d70b1c7fea220f015b84fdb3
Blake Laprade <lapradeblake@gmail.com>
2024-07-22 05:53:21 -0600

    added pool manager

---
+++ Assets/_KABOOM/Scripts/PoolObjects.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolObject : MonoBehaviour
+{
+    public void OnDeSpawn()
+    {
+        PoolManager.Instance.DeSpawn(this);
+    }
+
+    /// <summary>
+    /// called when spawned, in place of the start function
+    /// </summary>
+    public virtual void OnSpawn()
+    {
+        //GetComponent<SpriteRenderer>().color = GameManager.Instance.color;
+    }
+}
