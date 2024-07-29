80c464aae97266ecdaefc3c072f539da2f0cc71b
Blake Laprade <lapradeblake@gmail.com>
2024-07-29 10:05:23 -0600

    breakable tiles fixed

--- Assets/_KABOOM/Scripts/LevelObjects/BreakableTile.cs
+++ Assets/_KABOOM/Scripts/LevelObjects/BreakableTile.cs
@@ -6,12 +6,16 @@ using UnityEngine.Tilemaps;
 public class BreakableTile : MonoBehaviour
 {
     private Tilemap _breakableTilemap;
+    private Grid _grid;
 
     private float _compensationNumber = 0.01f;
+
+    
 
     private void Awake()
     {
         _breakableTilemap = GetComponent<Tilemap>();
+        _grid = GetComponentInParent<Grid>();
 
         if(_breakableTilemap == null)
         {
@@ -19,55 +23,31 @@ public class BreakableTile : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+           // _breakableTilemap.SetTile((, null);
+        }
+    }
+
+
     // This function should be called by an event
     public void BreakTile(Vector2 contactPoint)
     {
-        Debug.Log("BreakTile function called");
 
-        Vector2 hitPosition = Vector2.zero;
+        Vector3 hitPosition = Vector3.zero;
 
         HashSet<Vector3Int> brokenTiles = new HashSet<Vector3Int>(); // Used to avoid processing issues from duplicate hits
 
         hitPosition.x = contactPoint.x; //- _compensationNumber * contactPoint.normalized.x;
         hitPosition.y = contactPoint.y; //- _compensationNumber * contactPoint.normalized.y;
+        hitPosition.z = 0;
 
         Vector3Int currentCellPosition = _breakableTilemap.WorldToCell(hitPosition);
 
-        Debug.Log($"Hit position: {hitPosition}, Cell position: {currentCellPosition}");
+    
+        _breakableTilemap.SetTile(currentCellPosition, null);
 
-        if (!brokenTiles.Contains(currentCellPosition))
-        {
-            // Adds the broken tile to the HashSet
-            brokenTiles.Add(currentCellPosition);
-
-            // Sets the affected tile to null
-            _breakableTilemap.SetTile(currentCellPosition, null);
-
-            Debug.Log($"Tile located at {currentCellPosition} set to null");
-        }
-
-        /*
-        foreach (ContactPoint2D hit in collision.contacts)
-        {
-            // Find exactly what tile was hit in the tilemap
-            hitPosition.x = hit.point.x - _compensationNumber * hit.normal.x;
-            hitPosition.y = hit.point.y - _compensationNumber * hit.normal.y;
-
-            Vector3Int currentCellPosition = _breakableTilemap.WorldToCell(hitPosition);
-
-            Debug.Log($"Hit position: {hitPosition}, Cell position: {currentCellPosition}");
-
-            if (!brokenTiles.Contains(currentCellPosition))
-            {
-                // Adds the broken tile to the HashSet
-                brokenTiles.Add(currentCellPosition);
-
-                // Sets the affected tile to null
-                _breakableTilemap.SetTile(currentCellPosition, null);
-
-                Debug.Log($"Tile located at {currentCellPosition} set to null");
-            }
-        }
-        */
     }
 }
