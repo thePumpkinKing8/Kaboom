a85bdefa2af50e296d517ad1d83a3af9127aba96
Blake Laprade <lapradeblake@gmail.com>
2024-08-09 10:47:19 -0600

    AudioSO's  grabs random clips from list instead of manager

--- Assets/_KABOOM/Scripts/Audio/AudioSO.cs
+++ Assets/_KABOOM/Scripts/Audio/AudioSO.cs
@@ -6,7 +6,14 @@ using UnityEngine.Audio;
 [CreateAssetMenu(fileName = "AudioSO", menuName = "SOs/AudioSO")]
 public class AudioSO : ScriptableObject
 {
-    public AudioClip Clip;
+    public AudioClip Clip 
+    {  
+        get
+        {
+            return _clip[Random.Range(0, _clip.Length - 1)];
+        } 
+    }
+    [SerializeField] private AudioClip[] _clip;
 
     [Tooltip("Assign music and SFX to their own category mixer group.")]
     public AudioMixerGroup Mixer;
