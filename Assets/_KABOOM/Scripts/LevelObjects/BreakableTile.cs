1d36effd12204f9de8f8fcd7424a5ff9041135e6
Shaylyn Melin <shaylynmelin@gmail.com>
2024-08-11 23:01:17 -0600

    Tile breaking
    Removed delay for tile breaking + changed from coroutine to void function + cleaned up unused code inside BreakableTile.cs

--- Assets/_KABOOM/Scripts/LevelObjects/BreakableTile.cs
+++ Assets/_KABOOM/Scripts/LevelObjects/BreakableTile.cs
@@ -7,9 +7,6 @@ public class BreakableTile : MonoBehaviour
 {
     private Tilemap _breakableTilemap;
 
-    [SerializeField]
-    private float _tilebreakDelay = 2;
-
     private void Awake()
     {
         _breakableTilemap = GetComponent<Tilemap>();
@@ -20,37 +17,20 @@ public class BreakableTile : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-           // _breakableTilemap.SetTile((, null);
-        }
-    }
-
-
     // This function should be called by an event
     public void BreakTile(Vector2 contactPoint)
     {
-        Debug.Log("before");
-
-        StartCoroutine(TileBreakDelay(contactPoint));
-
-        Debug.Log("after");
-
+        TileBreak(contactPoint);
     }
 
-    // Change to coroutine again when we use the tilebreakdelay
-    IEnumerator TileBreakDelay(Vector2 contactPoint)
+    void TileBreak(Vector2 contactPoint)
     {
-        yield return new WaitForSeconds(_tilebreakDelay);
-
         Vector3 hitPosition = Vector3.zero;
 
         HashSet<Vector3Int> brokenTiles = new HashSet<Vector3Int>(); // Used to avoid processing issues from duplicate hits
 
-        hitPosition.x = contactPoint.x; //- _compensationNumber * contactPoint.normalized.x;
-        hitPosition.y = contactPoint.y; //- _compensationNumber * contactPoint.normalized.y;
+        hitPosition.x = contactPoint.x;
+        hitPosition.y = contactPoint.y;
         hitPosition.z = 0;
 
         Vector3Int currentCellPosition = _breakableTilemap.WorldToCell(hitPosition);
@@ -58,9 +38,5 @@ public class BreakableTile : MonoBehaviour
 
         _breakableTilemap.SetTile(currentCellPosition, null);
         PoolManager.Instance.Spawn("Explosion1");
-
-        //Debug.Log("coroutine finished");
-
-        //yield return null;
     }
 }
