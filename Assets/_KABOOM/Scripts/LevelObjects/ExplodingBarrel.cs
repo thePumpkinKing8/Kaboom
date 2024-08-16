104bc18a862cd3559b05f7bdf2f7c82385c1eaee
Blake Laprade <lapradeblake@gmail.com>
2024-08-15 23:12:42 -0600

    Checkpoints,Idestructables, effect fixes

--- Assets/_KABOOM/Scripts/LevelObjects/ExplodingBarrel.cs
+++ Assets/_KABOOM/Scripts/LevelObjects/ExplodingBarrel.cs
@@ -3,31 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
-public class ExplodingBarrel : BaseHealth, IHealth
+public class ExplodingBarrel :MonoBehaviour, IDestructable
 {
-    private GameObject _explosionTrigger;
-
-    GameObject FindChildWithTag(string tag)
-    {
-        // Looks for a specific child object with a tag
-        foreach (Transform child in transform)
-        {
-            if (child.CompareTag(tag))
-                return child.gameObject;
-        }
-
-        return null;
-    }
-
+    [SerializeField] protected float _explosionForce;
+    [SerializeField] protected float _explosionRadius;
     private void Awake()
     {
-        _explosionTrigger = FindChildWithTag("ExplosionTrigger"); // Finds the trigger object
-        _explosionTrigger.SetActive(false); // Sets the trigger as inactive
     }
 
     private void Start()
     {
-        this.CurrentHealthType = this;
+        
     }
 
     public void Health()
@@ -37,8 +23,23 @@ public class ExplodingBarrel : BaseHealth, IHealth
 
     public void Explode()
     {
-        _explosionTrigger.SetActive(true); // Sets the trigger as active
+        Debug.Log("explode");
+        //_explosionTrigger.SetActive(true); // Sets the trigger as active
+        Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, 20f, LayerMask.NameToLayer("Player"));
+        if(playerCollider)
+        {
+            playerCollider.gameObject.GetComponent<Rigidbody2D>().AddForce((playerCollider.transform.position - transform.position).normalized * _explosionForce);
+        }
 
+        RaycastHit2D[] circle = Physics2D.CircleCastAll(transform.position, 20f, Vector2.zero,LayerMask.NameToLayer("Breakable"));
+      
+
+        foreach(RaycastHit2D hit in circle)
+        {
+            hit.collider.GetComponent<BreakableTile>()?.BreakTile(hit.point);
+        }
+
+       
         // Some logic for explosions/event to trigger explosion logic here
 
         float explosionDelay = 0.1f; // Makes sure the object doesn't get destroyed before executing all its logic
@@ -46,13 +47,11 @@ public class ExplodingBarrel : BaseHealth, IHealth
         Destroy(this.gameObject, explosionDelay);
     }
 
-    // Testing
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.K))
-    //    {
-    //        this.TakeDamage(_damageScriptableObject.TurretDamage);
-    //    }
-    //}
 
+    public void ObjectDestroyed()
+    {
+        Explode();
+        float explosionDelay = 0.1f;
+        Destroy(this.gameObject, explosionDelay);
+    }
 }
--- Assets/_KABOOM/Scripts/Utilities/Singleton.cs
+++ Assets/_KABOOM/Scripts/Utilities/Singleton.cs
@@ -12,8 +12,34 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
             if (_instance == null)
             {
                 _instance = FindAnyObjectByType<T>();
+                DontDestroyOnLoad(_instance);
             }
+            
             return _instance;
         }
     }
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (this != _instance)
+            {
+                // If there is already an instance of this Singleton, destroy this one.
+                Destroy(gameObject);
+            }
+        }
+
+        OnAwake();
+    }
+
+    protected virtual void OnAwake()
+    {
+
+    }
+
 }
