04f08d0fb0eb560abb8653876f14af41b11fa7a6
Blake Laprade <lapradeblake@gmail.com>
2024-08-17 00:30:22 -0600

    PauseMenu added

--- Assets/_KABOOM/Scripts/Objects/EventObjects/PlayerActionsData.cs
+++ Assets/_KABOOM/Scripts/Objects/EventObjects/PlayerActionsData.cs
@@ -10,11 +10,14 @@ public class PlayerActionsData : ScriptableObject
     public UnityEvent PlayerShootEvent;
     public UnityEvent PlayerShootCancel;
     public UnityEvent<Vector2,bool> PlayerAimEvent;
+    public UnityEvent PlayerPauseEvent;
 
     public void HandlePlayerMovement(Vector2 movement) => PlayerMoveEvent?.Invoke(movement);
     public void HandlePlayerShoot() => PlayerShootEvent.Invoke();
     public void HandleShootCancel() => PlayerShootCancel.Invoke();
     public void HandlePlayerAim(Vector2 direction, bool isKBM) => PlayerAimEvent.Invoke(direction, isKBM);
+
+    public void PlayerPause() => PlayerPauseEvent.Invoke();
 
     
 }
--- Assets/_KABOOM/Scripts/UI/PauseMenuManager.cs
+++ Assets/_KABOOM/Scripts/UI/PauseMenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class PauseMenuManager : MonoBehaviour
 {
@@ -11,83 +12,86 @@ public class PauseMenuManager : MonoBehaviour
     [SerializeField]
     private GameObject _pauseMenu;
 
-    [SerializeField]
-    private GameObject _quitWarning;
-
-    [SerializeField]
-    private GameObject _saveMenu;
-
-    [SerializeField]
-    private GameObject _settingsMenu;
-
+    [SerializeField] private GameObject _controlsMenu;
+    [SerializeField] private GameObject _topButton;
+    private EventSystem _eventSystem;
     public bool IsPaused { get; private set; }
 
     #endregion
 
     private void Awake()
     {
-        CloseMenus();
+        _eventSystem = EventSystem.current;
+        _pauseMenu.SetActive(false);
+        _controlsMenu.SetActive(false);
+        InputManager.Instance.ActionsData.PlayerPauseEvent.AddListener(PauseInput);
     }
 
+    public void PauseInput()
+    {
+        if(IsPaused)
+        {
+            OnResume();
+        }
+        else
+        {
+            OnPause();
+        }
+    }
     public void OnPause()
     {
         Time.timeScale = 0f;
 
         _pauseMenu.SetActive(true);
-
+        StartCoroutine(OpenMenu());
+        InputManager.Instance.Input.Player.Disable();
         IsPaused = true;
     }
 
     public void OnResume()
     {
         _pauseMenu.SetActive(false);
-
+        InputManager.Instance.Input.Player.Enable();
         Time.timeScale = 1f;
 
         IsPaused = false;
     }
+    public void OnControls()
+    {
+        StartCoroutine(OpenControls());
+        _controlsMenu.SetActive(true);
+        _pauseMenu.SetActive(false);
+    }
 
     public void CloseMenus()
     {
-        if(_quitWarning.activeInHierarchy || _saveMenu.activeInHierarchy || _settingsMenu.activeInHierarchy)
-        {
-            // For closing menus within the pause menu
-            _settingsMenu.SetActive(false);
-            _saveMenu.SetActive(false);
-            _quitWarning.SetActive(false);
-            _pauseMenu.SetActive(true);
-        }
+        StartCoroutine(OpenMenu());
+        _pauseMenu.SetActive(true);
+        _controlsMenu.SetActive(false);
     }
 
     public void OnMainMenu()
     {
         // Showing the "do you want to quit" warning when the player selects Main Menu
-        _quitWarning.SetActive(true);
-    }
-
-    public void OnYesQuit()
-    {
-        // "No" should be linked to "close menus"
-
-        Time.timeScale = 1f; // Required to avoid bugs
-
-        IsPaused = false;
-
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void OnSave()
+    IEnumerator OpenControls()
     {
-        _saveMenu.SetActive(true);
-        _pauseMenu.SetActive(false);
+        _eventSystem.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        _eventSystem.SetSelectedGameObject(_controlsMenu);
+        yield return null;
+    }
+    IEnumerator OpenMenu()
+    {
+        _eventSystem.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        _eventSystem.SetSelectedGameObject(_topButton);
+        yield return null;
     }
 
-    public void OnSettings()
-    {
-        _pauseMenu.SetActive(false);
-
-        _settingsMenu.SetActive(true);
-    }
+    
 
 
 }
